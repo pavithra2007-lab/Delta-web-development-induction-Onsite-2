6446d1b0a6f3a02f544217e47f006fa46f83610c
Pavithra.M <pavithramuralikrishnan2007@gmail.com>
2026-07-30 22:46:49 +0530

    Refactor server to use Socket.IO for real-time updates

--- backend/server.js
+++ backend/server.js
@@ -3,35 +3,61 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { createServer } from 'node:http';
 import { Server } from 'socket.io';
-
 dotenv.config();
-
 const app = express();
 const server = createServer(app);
-
-const socket = new WebSocket("ws://localhost:5000");
+const io = new Server(server, {cors: {
+    origin: "http://localhost:5173",
+  },});
 
 app.use(cors());
 app.use(express.json());
+const gameState = {
+  sqr: { x: 100, y: 100,}, 
+  players: {},};
 
-io.on('connection', (socket) => {
-  console.log('a user connected:', socket.id);
-  socket.on('disconnect', function() {
-    console.log('user disconnected');
+let canvasWidth = 1000;
+let canvasHeight = 600;
+
+setInterval(() => {
+  gameState.sqr.x = Math.floor(Math.random() * canvasWidth);
+  gameState.sqr.y = Math.floor(Math.random() * canvasHeight);
+  io.emit("squareUpdate", gameState.sqr);
+}, 2000);
+
+io.on("connection", (socket) => {
+  socket.on("canvasSize", ({ width, height }) => {
+  canvasWidth = width;
+  canvasHeight = height;
+});
+  socket.emit("squareUpdate", gameState.sqr);
+  io.emit("scoreUpdate", gameState.players);
+  socket.on("newPlayer", ({ username }) => {gameState.players[socket.id] = {
+        username:username,score: 0,
+    };
+    io.emit("scoreUpdate", gameState.players);
+});
+  socket.on("hit", ({ x, y }) => {
+    const sq = gameState.sqr;
+    if ( x >= sq.x && x <= sq.x + 50 && y >= sq.y && y <= sq.y + 50
+    ) {
+      gameState.players[socket.id].score++;
+       if (gameState.players[socket.id].score >= 10) {
+      io.emit("winner", {username: gameState.players[socket.id].username,});
+    }
+      io.emit("scoreUpdate", gameState.players);
+    }
   });
-  socket.on('newPlayer', () => {
-  gameState.players[socket.id] = {
-    score:0,
-  }
+  socket.on("disconnect", () => {
+    delete gameState.players[socket.id];
+    io.emit("scoreUpdate", gameState.players);
+  });
 });
-});
-
 app.get("/", (req, res) => {
   res.send("Backend Running");
 });
-
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
